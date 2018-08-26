b7a5f2e11ea110da6d42252143b81e279918c1c6
dinara0708 <aikenova01@gmail.com>
2018-08-26 14:57:01 +0600

    Add project files.

---
+++ HW_pointers_26.08.2018/Source.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <iomanip>
+#include<math.h>
+#include<ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+	int tn = 0;
+
+	while (true)
+	{
+		cout << "Задача № ";
+		cin >> tn;
+
+		switch (tn)
+		{
+		case 1:
+		{
+
+		}
+		break;
+		case 2:
+		{
+
+		}
+		break;
+		case 3:
+		{
+
+		}
+		break;
+		case 4:
+		{
+
+		}
+		break;
+		case 5:
+		{
+
+		}
+		break;
+		case 6:
+		{
+
+		}
+		break;
+		case 7:
+		{
+
+		}
+		break;
+		default:
+			cout << "нет такой задачи" << endl << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
